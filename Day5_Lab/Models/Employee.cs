3f7a1330cdf665192521842c67f567ac21285922
No name <dinhcoong9@gmial.com>
2025-09-25 20:31:44 +0700

    nop bai day 5

---
+++ Day5_Lab/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day5_Lab.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public decimal Salary { get; set; }
+        public string Status { get; set; } = string.Empty;
+
+    }
+}
