e4bc0415004a9a3b9ace17dc831ddebcedeeb269
No name <dinhcoong9@gmial.com>
2025-10-11 21:23:43 +0700

    code-first

---
+++ data_access_2/Models/BusinessModels/BookManagementContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using data_access_2.Models.DataModels;
+
+namespace data_access_2.Models.BusinessModels
+{
+    public class BookManagementContext: DbContext
+    {
+        public BookManagementContext(DbContextOptions<BookManagementContext> options) : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+    }
+    
+
+    
+}
